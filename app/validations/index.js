f5d936f2e0b75916b7220fd6dd5827f19e7e5c23
cyrille <aviateur22@hotmail.fr>
2022-03-21 16:35:22 +0100

    merge user on dev

--- app/validations/index.js
+++ app/validations/index.js
@@ -4,11 +4,10 @@
  * @returns {object} - next si validation OK ou error
  */
 module.exports = (schema) =>async(req, res, next) =>{
-    try {          
-        console.log(req.body);  
+    try {         
         await schema.validateAsync(req.body);
         next();        
     } catch (error) {           
-        next({message: error.message, statusCode:'422', resetAuth: false , redirect :'/', error: true});
+        next({message: error.message, statusCode:'422'});
     }
 };
