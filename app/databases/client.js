b11841e3a290df262c9887dbaaffa34180e1335f
cyrille <aviateur22@hotmail.fr>
2022-04-04 16:54:46 +0200

    update sequelize production connection pool

--- app/databases/client.js
+++ app/databases/client.js
@@ -1,10 +1,40 @@
 const { Sequelize }  = require('sequelize');
 
-const sequilize = new Sequelize(process.env.DATABASE_URL,{
-    define:{
-        underscored:true,
-        updatedAt: 'updated_at',
-        createdAt: 'created_at'
-    }    
-});
-module.exports=sequilize;
+let sequelize;
+
+/** connection en production */
+if(process.env.NODE_ENV === 'production'){
+    sequelize = new Sequelize(process.env.DATABASE_URL,        
+        {
+            dialectOptions:{
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            },
+            define:{
+                underscored:true,
+                updatedAt: 'updated_at',
+                createdAt: 'created_at'            
+            }
+        });
+} else {
+    /** connection en local */
+    sequelize = new Sequelize(process.env.DATABASE_URL,{
+        define:{
+            underscored:true,
+            updatedAt: 'updated_at',
+            createdAt: 'created_at'
+        }
+    });
+}
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+module.exports=sequelize;
