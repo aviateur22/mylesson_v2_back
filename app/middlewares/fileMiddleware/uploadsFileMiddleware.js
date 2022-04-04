bfeaffbda810ea5002f95d995d22dbbe00800042
cyrille <aviateur22@hotmail.fr>
2022-04-04 23:45:54 +0200

    update upload file folder

--- app/middlewares/fileMiddleware/uploadsFileMiddleware.js
+++ app/middlewares/fileMiddleware/uploadsFileMiddleware.js
@@ -12,7 +12,7 @@ const upload = {
         /** verification du format du document et parametrage de uploads path */
         destination: async function(req, file, cb) {                       
             if(file.mimetype === 'image/png' || file.mimetype === 'image/jpeg'){                
-                (null,path.join(__dirname, process.env.UPLOAD_PATH));                
+                (null, __dirname + 'files/upload/');                
             } else {
                 /** le format de l'image n'est pas correcte */
                 cb({message: 'seules les images au format JPEG et PNG sont acceptées', statusCode:'400'});
