b15f08c61a91c69a48597fa8131336f2d385275b
cyrille <aviateur22@hotmail.fr>
2022-04-05 11:07:14 +0200

    update upload file folder

--- app/middlewares/fileMiddleware/uploadsFileMiddleware.js
+++ app/middlewares/fileMiddleware/uploadsFileMiddleware.js
@@ -12,20 +12,16 @@ const upload = {
         /** verification du format du document et parametrage de uploads path */
         destination: async function(req, file, cb) {                       
             if(file.mimetype === 'image/png' || file.mimetype === 'image/jpeg'){    
-                console.log('ici');                                   
-                cb(null, path.join(process.cwd(), process.env.UPLOAD_PATH_IMAGE));    
-                console.log(path.join(process.cwd(), process.env.UPLOAD_PATH_IMAGE));                                   
+                cb(null, path.join(process.cwd(), process.env.UPLOAD_PATH_IMAGE));                    
             } else {
                 /** le format de l'image n'est pas correcte */
                 cb({message: 'seules les images au format JPEG et PNG sont acceptées', statusCode:'400'});
             }        
         },
         /** si le format est validé on génére un identifiant unique est on la stock */
-        filename: function(req, file, cb) { 
-            console.log('uniqueSuffix');                
+        filename: function(req, file, cb) {             
             /** genration d'un uuid pour l'image */
-            const uniqueSuffix = uuidGenerator();
-            console.log(uniqueSuffix);
+            const uniqueSuffix = uuidGenerator();            
             cb(null, file.fieldname + '-' + uniqueSuffix +'.png');                       
         }
     })
